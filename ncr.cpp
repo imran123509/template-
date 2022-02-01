59506d05d21c801b7431a8113b1a98250deef566
Imran hassan <72485040+imran123509@users.noreply.github.com>
2022-02-01 21:55:22 +0530

    Add files via upload

---
+++ ncr.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll nCr(int n, int r){
+	if(r == 0)	return 1;
+	return nCr(n-1, r-1) * n/r;
+}
+
+int main(){
+    cout << nCr(5, 3) << endl;
+    return 0;
+}
